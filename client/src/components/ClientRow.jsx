1c48d7677b36bb39d866fa51c51a894d9e5a2f4a
Ivan Skordev <maymunskoa@yahoo.com>
2022-08-07 02:38:04 +0300

    add client modal and query

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,6 +1,22 @@
 import { FaTrash } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
 
 export default function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    // refetchQueries: [{ query: GET_CLIENTS }],
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id !== deleteClient.id),
+        },
+      });
+    },
+  });
   const { name, email, phone } = client;
   return (
     <tr>
@@ -8,7 +24,7 @@ export default function ClientRow({ client }) {
       <td>{email}</td>
       <td>{phone}</td>
       <td>
-        <button className='btn btn-danger btn-sm'>
+        <button className='btn btn-danger btn-sm' onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
