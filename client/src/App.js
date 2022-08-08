4fa217bed817dcc29b8a47de40d7d97ba27c494a
Ivan Skordev <maymunskoa@yahoo.com>
2022-08-08 19:50:07 +0300

    display projects and single project details

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import Header from './components/Header';
-import Clients from './components/Clients';
-import AddClientModal from './components/AddClientModal';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+import Header from './components/Header';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Project from './pages/Project';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -31,11 +33,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className='container'>
-          <AddClientModal />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project/>}/>
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
